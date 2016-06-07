41e2ba5f9b672cc06d71a4013de35a32edeef69e
Alex Roth <aisaacroth@gmail.com>
2016-06-07 15:20:09 -0400

    Working on the server.

--- server/server.go
+++ server/server.go
@@ -2,12 +2,18 @@ package main
 
 import (
     "fmt"
+    "net"
     "os"
+    "strings"
     "sync"
 )
 
 var (
     once sync.Once
+)
+
+const (
+    HOME = "127.0.0.1"
 )
 
 func main() {
@@ -23,10 +29,20 @@ func main() {
         exit(0, nil)
     }()
 
+    address := strings.Join([]string{HOME, os.Args[1]}, "/")
+
+    _, err := net.Listen("tcp", address)
+    if err != nil {
+    }
+
+    fmt.Println("Server started at", address)
+
+
     // 1a. Server reads in from a file of possible safe users?
     // 1b. Server can ask users to register with the service.
 
     // 2a. Server listens on given port number for any incoming connections
+
     // 2b. Receives a request from client to access the system
 
     // 3a. Send response requesting username and password
