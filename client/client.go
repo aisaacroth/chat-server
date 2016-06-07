fa4871bb8cf54f983e30efc75125dd8919dc856f
Alex Roth <aisaacroth@gmail.com>
2016-06-07 14:46:03 -0400

    Working on stuff

--- client/client.go
+++ client/client.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+    "fmt"
+    "os"
+    "sync"
+)
 
+var (
+    once sync.Once
+)
+
+func main() {
+    if len(os.Args) < 3 {
+        fmt.Println("client <ip> <port>")
+        exit(0, nil)
+    }
+
+    defer func() {
+        if err := recover(); err != nil {
+            exit(1, err.(error))
+        }
+        exit(0, nil)
+    }()
+
+    // 1a. Request a connection to the server on the given IP/Port
+    // 1b. Response from server should ask for credentials
+
+    // 2a. Client sends credentials
+    // 2b. Response from server is either verified or not. 
+    // 2c. If verified, access to the server.
+    // 2d. If not verified, drop the connection.
+
+    // 3a. On connection, client can send messages to the server
 }
+
+func exit(code int, err error) {
+    once.Do(func() {
+        if err != nil {
+            fmt.Fprintln(os.Stderr, err.Error())
+        }
+
+        os.Exit(code)
+    })
+}
+
--- server/server.go
+++ server/server.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+    "fmt"
+    "os"
+    "sync"
+)
 
+var (
+    once sync.Once
+)
+
+func main() {
+    if len(os.Args) < 2 {
+        fmt.Println("server <port num>")
+        exit(0, nil)
+    }
+
+    defer func() {
+        if err := recover(); err != nil {
+            exit(1, err.(error))
+        }
+        exit(0, nil)
+    }()
+
+    // 1a. Server reads in from a file of possible safe users?
+    // 1b. Server can ask users to register with the service.
+
+    // 2a. Server listens on given port number for any incoming connections
+    // 2b. Receives a request from client to access the system
+
+    // 3a. Send response requesting username and password
+    // 3b. Receive response with username and password, verify against
+    //     known users.
+    // 3c. If the user does not validate self within 5 attempts, close
+    //     the connection.
 }
+
+func exit(code int, err error) {
+    once.Do(func() {
+        if err != nil {
+            fmt.Fprintln(os.Stderr, err.Error())
+        }
+
+        os.Exit(code)
+    })
+}
+
