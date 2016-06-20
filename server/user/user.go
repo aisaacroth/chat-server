2513d796c7d9eab569b2a323560453fbd2611376
Alex Roth <aisaacroth@gmail.com>
2016-06-20 17:47:12 -0400

    Updated user.

--- server/user/user.go
+++ server/user/user.go
@@ -5,11 +5,12 @@ import (
 )
 
 type User struct {
-	Name    string
-	Address net.Addr
+	Name     string
+	Address  net.Addr
+	Password string
 }
 
 // String representation of user
 func (u User) String() string {
-    return "{" + string(u.Name) + ":" + u.Address.String() + "}"
+	return "{" + string(u.Name) + ":" + u.Address.String() + "}"
 }
